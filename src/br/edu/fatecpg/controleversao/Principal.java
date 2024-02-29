c4791bca653689dc0493ec5367b91b3f74db186f
Vitor <vitor@gmail.com>
2024-02-29 16:18:09 -0300

    Criação do projeto Java

---
+++ src/br/edu/fatecpg/controleversao/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.fatecpg.controleversao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	
+		System.out.println("Hello World!");
+		System.out.println("Hello GitHub!");
+	}
+
+}
